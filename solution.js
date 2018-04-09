ae9112c431a5e1917a4592f658691395f902fa1a
sinclairstudy <sinclairstudy@gmail.com>
2018-04-09 09:50:57 -0400

    math.max

--- solution.js
+++ solution.js
@@ -7,20 +7,32 @@ function checksum (spreadsheet){
   for (let i = 0; i < rows; i++){
     const row = spreadsheet[i]
 
-    let min = row[0]
-    let max = row[0]
+    // let min = row[0]
+    // let max = row[0]
     for (let j = 0; j < columns; j++){
-      if (row[j] < min){
-        min = row[j]
-      } else if (row[j] > max){
-        max = row[j]
-      }
+      // if (row[j] < min){
+      //   min = row[j]
+      // } else if (row[j] > max){
+      //   max = row[j]
+      // }
     }
-    checksum += max - min
+    checksum += Math.max(...row) - Math.min(...row)
   }
 
-
-
-
   return checksum
+
+
+  // [
+  //   [409, 194, 207, 470, 178],
+  //   [454, 235, 333, 511, 103],
+  //   [474, 293, 525, 372, 408],
+  //   [428, 4321, 2786, 6683, 3921],
+  //   [265, 262, 6206, 2207, 5712]
+  // ]
+  //
+  // => 13131
+
+
+
+
 }
