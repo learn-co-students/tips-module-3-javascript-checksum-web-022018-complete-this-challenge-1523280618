cb068b3b00ab4fcfa2bdc3b5031341a6a50e19f3
github username <github email address>
2018-04-09 09:53:02 -0400

    better solution

--- solution.js
+++ solution.js
@@ -1,9 +1,11 @@
 function checksum (arr){
   let total = 0
   for (const el of arr){
-    el.sort()
+    el.sort((num1, num2) => (num1-num2))
+    console.log(el)
     let diff = el[el.length-1] - el[0]
     total += diff
   }
   return total
+
 }
